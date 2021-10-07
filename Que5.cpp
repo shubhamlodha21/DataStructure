e4d2cc2b89f5c304f31dce8475f8ba2d56350bc9
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-10-07 15:46:17 +0530

    Add files via upload

---
+++ Que5.cpp
@@ -0,0 +1,49 @@
+/*input:- s[]={"geeks","for","skeeg"}
+output:- geeks
+description:The task is to check the reverse of the particular word is present that array itself of or not
+date: 30-09-2021
+Author name: Shruti Nahar*/
+#include<iostream>
+using namespace std;
+bool reversestr(string s1,string s2)
+{
+    int len=s1.length();
+    if(s1.length()!=s2.length())
+    {
+        return false;
+    }
+
+        for(int i=0;i<len;i++)
+        {
+            if(s1[i]==s2[len-i-1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    return false;
+}
+string getwords(string s[],int n)
+{
+     for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(reversestr(s[i],s[j]))
+            {
+               return s[i];
+            }
+           
+        }
+    }
+    return "-1";
+}
+int main()
+{
+    string s[]={"geeks","for","skeeg"};
+    int n=s->length();
+   cout<<getwords(s,n);
+}
